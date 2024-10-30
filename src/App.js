46b48117d51adea8b39690c2ec7cff50a4c2737e
suryakiran9959 <surya.mail.net@gmail.com>
2024-10-31 00:08:25 +0530

    ninth component added and minor changes done on other components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Eight from './components/eight';
 import Five from './components/five';
 import Four from './components/four';
+import Nine from './components/nine';
 import One from './components/one';
 import Seven from './components/seven';
 import Six from './components/six';
@@ -20,6 +21,7 @@ function App() {
    <Six/>
    <Seven/>
    <Eight/>
+   <Nine/>
    </>
   )
 }
---
+++ src/components/nine.jsx
@@ -0,0 +1,13 @@
+import "./nine.css"
+function Nine(){
+    return(
+        <>
+        <div className="explore">
+                <h3>Explore Ours <span className="dapp">dApp</span> </h3>
+                <p className="ethai">EthAi is an AI-powered dApp designed to help traders make smarter, data-driven decisions. By tracking smart money flows, monitoring key wallets, and providing real-time market insights, EthAi empowers users to stay ahead of trends. The platform offers intuitive AI features for asset recommendations, market analysis, and personalized crypto Q&A, making it the ultimate tool for both novice and experienced traders.</p> 
+                <p className="open9">Open dApp</p>
+        </div>
+        </>
+    )
+}
+export default Nine;
